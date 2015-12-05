8eb5c264462a79943c93c901b4337f407a8e2bce
Bryden-Lacelle <lace0086@algonquinlive.com>
2015-12-05 11:45:49 -0500

    Update Account.cpp

--- Account.cpp
+++ Account.cpp
@@ -6,9 +6,9 @@ Student Number:		040754048
 Course Name:		C++ Programming CST8219
 Lab Section:		301
 Assignment Number:	1
-Assignment Name:	FleaBay in C++
-Due Date:			2015/10/19
-Submission Date:	2015/10/20
+Assignment Name:	FleaBay with Overloaded Operators
+Due Date:			2015/11/08
+Submission Date:	2015/11/09
 Professor's Name:	Andrew Taylor
 Purpose:			To manage Account objects and add Items to Accounts
 ***************************************************************************************************/
@@ -20,6 +20,7 @@ Purpose:			To manage Account objects and add Items to Accounts
 #include <iomanip>
 #define IOMANIP
 #endif // !IOMANIP
+using namespace std;
 #ifndef ITEM_H
 #include "Item.h"
 #define ITEM_H
@@ -28,11 +29,6 @@ Purpose:			To manage Account objects and add Items to Accounts
 #include "Account.h"
 #define ACCOUNT_H
 #endif // !ACCOUNT_H
-#ifndef FLEA_H
-#include "Fleabay.h"
-#define FLEA_H
-#endif // !ACCOUNT_H
-using namespace std;
 
 /*******************************************************************************
 Function Name:		Account::Account
@@ -45,9 +41,44 @@ Account::Account()
 	Account::ID = NULL;
 	Account::PassWord = NULL;
 	Account::numItems = 0;
-	Account::items = new pItem;
+	Account::items = NULL;
 }
 
+/*******************************************************************************
+Function Name:		Account::Account
+Purpose:			Copy Constructor for an Account
+In Parameters:		Account&
+Version:			1.0
+Author:				Bryden Lacelle
+*******************************************************************************/
+Account::Account(Account& account)
+{
+	int i = -1;
+	ID = new char[strlen(account.ID) + 1]; // Allocate memory for ID
+	strcpy(ID, account.ID); // Copy ID
+	PassWord = new char[strlen(account.PassWord) + 1];
+	strcpy(PassWord, account.PassWord);
+	numItems = account.numItems;
+	items = new pItem[numItems]; // Allocate memory for items
+	while (++i < account.numItems) { items[i] = new Item(*account.items[i]); } // Copy Items
+}
+
+/*******************************************************************************
+Function Name:		Account::Account
+Purpose:			Overloaded Constructor for a new Account
+In Parameters:		char*, char*
+Version:			1.0
+Author:				Bryden Lacelle
+*******************************************************************************/
+Account::Account(char* ID, char* password)
+{
+	Account::ID = new char[strlen(ID) + 1];
+	Account::PassWord = new char[strlen(password) + 1];
+	strcpy(Account::ID, ID);
+	strcpy(Account::PassWord, password);
+	Account::numItems = 0;
+	Account::items = new pItem;
+}
 /*******************************************************************************
 Function Name:		Account::AddItem
 Purpose:			Add a(n) items(s) to an Account
@@ -57,43 +88,56 @@ Author:				Bryden Lacelle
 void Account::AddItem()
 {
 	int maxIDSize = 50, i = -1;
+	char* description;
+	double price;
 	char tempChar, *pTempChar = new char[50]; // Allocate a temporary buffer for user input
 	bool addNItem = true;
 	ppItem oldItemList; // Create a new temporary ppItem 
 	cout << "These are your current items for sale:" << endl;
 	while(++i < numItems) 
-	items[i]->Report();
+		cout << *items[i];
 	if (!numItems)
 		cout << "**NO ITEMS FOUND**" <<  endl;
 	while (addNItem)
 	{
-		cout << "Do you wish to add another Item (yes = 'Y', no = 'N'?" << endl;
+		cout << "Do you wish to change a password (yes = 'P')?" << endl;
+		cout << "Do you wish to add another Item (yes = 'Y')?" << endl;
+		cout << "Want to quit this menu (yes = 'Q')?" << endl;
 		cin >> tempChar;
 		i = -1; // Reset i
 		switch(tempChar)
 		{
-		case 'N':
+		case 'Q':
 			addNItem = false;
 			break;
 		case 'Y':
 			oldItemList = items; // Set pointer to the old list of pointers
 			items = new pItem[numItems + 1]; // Allocate memory for a new list of pointers
 			while (++i < numItems) { items[i] = oldItemList[i]; } // Copy the old list of pointers over to the newly allocated space in memory
-			items[numItems] = new Item; // Allocate memory for a new Item
 			cout << "Please enter new item description: ";
 			cin.ignore();
-			cin.get(pTempChar, maxIDSize);
-			items[numItems]->description = new char[strlen(pTempChar) + 1]; // Allocate the exact amount of memory needed for the user's item description
-			strcpy(items[numItems]->description, pTempChar);
+			cin.getline(pTempChar, maxIDSize);
+			description = new char[strlen(pTempChar) + 1]; // Allocate the exact amount of memory needed for the user's item description
+			strcpy(description, pTempChar);
 			do // Loop until valid input is found
 			{
 				cin.clear(); // Reset error flags
 				cout << "Please Enter the new item price: ";
-				cin >> items[numItems]->price;
+				cin >> price;
 				cin.sync(); // Clear cin
 			} while(cin.fail());
+			items[numItems] = new Item(description, price); //Create a new item
 			++numItems; // Incriment the number of items stored in the database
 			delete oldItemList; // Delete the old list of pointers
+			delete description;
+			break;
+		case 'P':
+			cout << "Please enter a new password: ";
+			cin.ignore();
+			cin.getline(pTempChar, maxIDSize);
+			delete PassWord; // delete old Password
+			PassWord = new char[strlen(pTempChar) + 1]; // Allocate memory for new password
+			strcpy(PassWord, pTempChar); // Copy password to PassWord
 			break;
 		default:
 			cout << "Invalid input" << endl;
@@ -104,22 +148,35 @@ void Account::AddItem()
 }
 
 /*******************************************************************************
-Function Name:		Account::Report
-Purpose:			Display all information for a single account
+Function Name:		Account::getPassword
+Purpose:			Return value of password
+Out Parameters:		char*
 Version:			1.0
 Author:				Bryden Lacelle
 *******************************************************************************/
-void Account::Report()
+char*& Account::getPassWord()
 {
-	int i = -1;
-	cout << (!numItems ? "**NO ITEMS FOUND**" : "") << endl; // Return an error message if no items are found
-	while(++i < numItems) // Incriment through all the stored account descriptions and prices and print them to the console
-	{
-		cout << "Item " << i << ":";
-		items[i]->Report();
-	}
+	return PassWord;
 }
 
+/*******************************************************************************
+Function Name:		operator<<
+Purpose:			Overload the ostream << operator to print an account to the console
+In Parameters:		ostream&, Account&
+Out Parameters		ostream&
+Version:			1.0
+Author:				Bryden Lacelle
+*******************************************************************************/
+ostream& operator<<(ostream& stream, Account& account)
+{
+	int i = -1;
+	stream << "Account ID: " << account.ID << endl;
+	while(++i < account.numItems) // Incriment through all the stored account descriptions and prices and print them to the console
+	{
+		stream << "Item " << i << ":" << *account.items[i];
+	}
+	return stream;
+}
 /*******************************************************************************
 Function Name:		Account::~Account
 Purpose:			Destructor for an Account
