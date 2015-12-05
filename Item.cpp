5840ef53e0c2de5770af78a48dd2b2dc13415d7c
Bryden-Lacelle <lace0086@algonquinlive.com>
2015-12-05 11:48:52 -0500

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -6,9 +6,9 @@ Student Number:		040754048
 Course Name:		C++ Programming CST8219
 Lab Section:		301
 Assignment Number:	1
-Assignment Name:	FleaBay in C++
-Due Date:			2015/10/19
-Submission Date:	2015/10/20
+Assignment Name:	FleaBay with Overloaded Operators
+Due Date:			2015/11/08
+Submission Date:	2015/11/09
 Professor's Name:	Andrew Taylor
 Purpose:			To manage the use of an Item object
 ***************************************************************************************************/
@@ -20,11 +20,11 @@ Purpose:			To manage the use of an Item object
 #include <iomanip>
 #define IOMANIP
 #endif // !IOMANIP
+using namespace std;
 #ifndef ITEM_H
 #include "Item.h"
 #define ITEM_H
 #endif // !ITEM_H
-using namespace std;
 
 /*******************************************************************************
 Function Name:		Item::Item
@@ -39,16 +39,46 @@ Item::Item()
 }
 
 /*******************************************************************************
-Function Name:		Item::Report
-Purpose:			Display all information for a single item
+Function Name:		Item::Item
+Purpose:			Copy Constructor for an Item 
+In Parameters		&item
 Version:			1.0
 Author:				Bryden Lacelle
 *******************************************************************************/
-void Item::Report()
+Item::Item(Item& item)
 {
-	cout << "   " << description << ", Cost = $" << setiosflags(ios::showpoint) << price << endl;
+	description = new char[strlen(item.description) + 1];
+	strcpy(description, item.description);
+	price = item.price;
 }
 
+/*******************************************************************************
+Function Name:		Item::Item
+Purpose:			Constructor for a new Item with 2 parameters
+In parameters		char*, double
+Version:			1.0
+Author:				Bryden Lacelle
+*******************************************************************************/
+Item::Item(char* description, double price)
+{
+	Item::description = new char[strlen(description) + 1];
+	strcpy(Item::description, description);
+	Item::price = price;
+}
+
+/*******************************************************************************
+Function Name:		operator<<
+Purpose:			Overload the ostream << operator to print an item to the console
+In Parameters:		ostream&, Item&
+Out Parameters		ostream&
+Version:			1.0
+Author:				Bryden Lacelle
+*******************************************************************************/
+ostream& operator<<(ostream& stream, Item& item)
+{
+	stream << "   " << item.description << ", Cost = $" << setiosflags(ios::showpoint) << item.price << endl;
+	return stream;
+}
 /*******************************************************************************
 Function Name:		Item::~Item
 Purpose:			Destructor for an Item
